016179a0b3fe51b287060e978050d7c642c2dce0
vsanta <vanessa.santanna@gmail.com>
2011-04-21 14:47:17 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripapalooza::Application.config.secret_token = '23fa037311201e916e806e08879e56e162253541808debc5af9afa812d37fedeb60e80fc81ca08e4ec3774e83b72ea84a8143ea0aaa196e7a50928f3767fc9b7'
