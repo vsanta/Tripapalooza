298550436a6c2ba06f0020a051939ac6e792d868
vsanta <vanessa.santanna@gmail.com>
2011-04-22 16:29:02 -0300

    validation of required data for Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,11 @@
 class Trip < ActiveRecord::Base
+  validate :start_date_is_before_end_date
+
+  validates_presence_of :name, :destination, :destination_lat, :destination_lon
+
+
+
+  def start_date_is_before_end_date
+    self.start > self.end
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
+require 'date'
 
 describe Trip do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "has minimal data for a Trip" do
+    before(:each) do
+      @trip = Trip.new()
+      @trip.destination = "New York"
+      @trip.destination_lat = 40.77
+      @trip.destination_lon =  73.98
+      @today = Date.today()
+      @trip.start = @today
+      @trip.end = @today.next_month()
+      @trip.name = "My Cool Trip to NYC"
+    end
+    it "should have a name" do
+      @trip.name = nil
+      @trip.save.should raise_error
+      @trip.name = ""
+      @trip.save.should raise_error
+    end
+    describe "destination information" do
+      it "should have destination's name" do
+        @trip.destination = nil
+        @trip.save.should raise_error
+        @trip.destination = ""
+        @trip.save.should raise_error
+      end
+      it "should have lat and lon based on destination" do
+        pending
+      end
+    end
+    describe "date information" do
+      it "should have start date before of end date" do
+        @trip.end = @today
+        @trip.start = @today.next_month()
+        @trip.save.should raise_error
+      end
+    end
+  end
 end
